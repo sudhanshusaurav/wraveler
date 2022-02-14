2da5ad6d865918b1e012c91d7d6d30057ec2ff91
sudhanshu_saurav <sudhanshussaurav@gmail.com>
2022-02-15 02:40:19 +0530

    started

--- src/services/TripServices.js
+++ src/services/TripServices.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export async function getAllTrips() {
   try {
     const res = await axios.get(
-      'https://cors-anywhere.herokuapp.com/https://catfact.ninja/facthttps://www.justwravel.com/otherPages/month_trip_api/February/all'
+      'https://catfact.ninja/facthttps://www.justwravel.com/otherPages/month_trip_api/February/all'
     );
     return res;
   } catch (error) {
